ec70b5cb9f0b270d72147f690121d01aba837c06
mizore-fu <mhxxname0782@gmail.com>
2024-01-04 11:23:33 +0900

    JSONデータを構造体に変換

--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 )
 
+type Weather struct {
+	Daily struct {
+		Time []string `json:"time"`
+		WeatherCode []int `json:"weather_code"`
+	} `json:"daily"`
+}
 
 func main() {
-
 	url := "https://api.open-meteo.com/v1/forecast?latitude=35.6895&longitude=139.6917&daily=weather_code&timezone=Asia%2FTokyo"
 	res, err := http.Get(url)
 	if err != nil {
@@ -25,5 +31,10 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Println(string(body))
+	var weather Weather
+	err = json.Unmarshal(body, &weather)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(weather)
 }
